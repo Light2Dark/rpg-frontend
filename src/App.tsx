2205d593dd564e775e3b152806b2487063deab7a
Shahmir <sham9871@gmail.com>
2022-09-03 13:49:16 +0800

    added base template for hacker-news style app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 function App() {
 
   return (
-    <div>
-      
+    <div className="w-4/5 m-auto mt-4">
+
+      <div className="flex flex-row items-center gap-4 bg-orange-400">
+        <h1 className="font-formal font-bold text-lg pl-2">Sun Confessions</h1>
+        <a href="#" className="font-formal text-sm">post</a>
+        <a href="#" className="font-formal text-sm">login</a>
+      </div>
+
+      <div className= "bg-[#F6F6EF]/80 px-2 py-2">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam, rerum?</p>
+      </div>
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,10 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      "formal": "Merriweather",
+      "informal": "Ubuntu"
+    }
   },
   plugins: [],
 }
