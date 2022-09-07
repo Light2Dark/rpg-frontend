c30934a12f1573dd7412036d8cba1c7f005f25b9
Shahmir <sham9871@gmail.com>
2022-09-07 23:29:24 +0800

    installed urql, digest the apis to run when app is running

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import Confession from "./components/Confessions"
-import { Post, User } from "./types"
-
-const posts: Post[] = [
-  {
-    body: "I discovered Tortuga",
-    author: {name: "Alfonso Alburquerque"}
-  },
-  {
-    body: "I pioneered computer science",
-    author: {name: "Ada Lovelace"}
-  }
-]
+import {useQuery} from "urql"
+import { GetUsersDocument } from "./graphql/generated"
 
 function App() {
+
+  // query the api
+  const [results] = useQuery({
+    query: GetUsersDocument
+  })
 
   return (
     <div className="w-4/5 m-auto mt-4">
@@ -24,7 +19,9 @@ function App() {
       </div>
 
       <div className= "bg-[#F6F6EF]/80 px-2 py-2">
-        <Confession content={posts} />
+        {
+          results.data?.users.map((user, index) => <Confession key={index} content={user.posts} />)
+        }
       </div>
     </div>
   )
